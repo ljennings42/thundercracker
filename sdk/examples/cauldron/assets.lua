4cb3a5b185da417d8ad994ab1e80a24809a23d8e
leahjennings <leahjennings@outlook.com>
2024-01-27 16:31:37 -0500

    Added updated lua

--- sdk/examples/cauldron/assets.lua
+++ sdk/examples/cauldron/assets.lua
@@ -1,4 +1,23 @@
 -- Metadata
 
 IconAssets = group{quality=9.95}
-Icon = image{"icon.png"}
+Icon = image{"images/icon.png"}
+
+-- Loading screen: Divided into two asset groups
+
+BootstrapGroup = group{ quality=9.8 }
+LoadingBg = image{ "images/loading.png" }
+
+function frames(fmt, count)
+    t = {}
+    for i = 1, count do
+        if (i % 2 == 0)
+        then
+           t[1+#t] = string.format(fmt, i)
+        end
+    end
+    return t
+end
+
+CauldronGroup = group{quality=8}
+Cauldron = image{ frames("images/Cauldron_Frames/Cauldron_1.%04d.png", 59)}
